ceeb342f7690bf5bd925d405eeef229528133f18
takatoshi.tenko <chanten811@gmail.com>
2023-08-29 23:35:03 +0900

    add: pages配下に最初の画面を表示させる

--- alligator/pages/index.vue
+++ alligator/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    今日の日付
+    <h2>今日の日付</h2>
+    <p class="today">today</p>
+    <p>目標の日付：<input type="date" name="date" /></p>
+    <p>入力した日付まで後<span class="leftDay"></span>日</p>
   </div>
 </template>
