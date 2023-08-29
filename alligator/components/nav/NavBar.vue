b81ddf1fa2cc2866adf223a242607874dcf4aaab
takatoshi.tenko <chanten811@gmail.com>
2023-08-30 00:01:03 +0900

    add: ナビコンポーネント作成

---
+++ alligator/components/nav/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <NuxtLink href="/">Home</NuxtLink>
+    <NuxtLink href="/about">About</NuxtLink>
+    <NuxtLink to="https://google.com" target="_blank">
+      Google
+    </NuxtLink>
+  </nav>
+</template>
--- alligator/layouts/default.vue
+++ alligator/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav>ここにナビゲーションバーを入れる</nav>
+    <NavBar />
     <slot />
     <p>ここがフッターになる</p>
   </div>
