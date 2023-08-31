039a362b8f826d757659a4b86ceb66e63c3ddd2a
takatoshi.tenko <chanten811@gmail.com>
2023-08-31 23:43:13 +0900

    add: cssファイルパス、meta情報の更新

--- alligator/nuxt.config.ts
+++ alligator/nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ["~/assets/css/style.css"],
+  app: {
+    head: {
+      title: "Nuxt 3 basic from Takatoshi Tenko",
+      meta: [{ name: "description", content: "Nuxt 3 for beginners" }],
+      link: [{ rel: "icon", href: "/public/favicon.ico" }],
+    },
+  },
   components: [
     {
       path: "~/components",
