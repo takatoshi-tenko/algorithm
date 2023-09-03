ec586a33b1322bfeb3bdacbb699c00127beaed9d
takatoshi.tenko <chanten811@gmail.com>
2023-09-03 09:41:41 +0900

    add: post listナビ追加

--- alligator/components/nav/NavBar.vue
+++ alligator/components/nav/NavBar.vue
@@ -4,5 +4,6 @@
     <NuxtLink href="/about">About</NuxtLink>
     <NuxtLink to="https://google.com" target="_blank"> Google </NuxtLink>
     <NuxtLink :to="{ name: 'users-list' }">User List</NuxtLink>
+    <NuxtLink to="/posts">Post List</NuxtLink>
   </nav>
 </template>
