c69fb01470fe06aab80a04360b863139d934ef2f
takatoshi.tenko <chanten811@gmail.com>
2023-09-03 09:41:54 +0900

    add: posts/index.vueの追加

---
+++ alligator/pages/posts/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+const {
+  data: posts,
+  error,
+  refresh,
+} = await useFetch("https://jsonplaceholder.typicode.com/posts/");
+console.log(error.value, "error");
+</script>
+
+<template>
+  <div>
+    <h1>Posts一覧</h1>
+    <button @click="refresh()">再取得</button>
+    <p v-if="error">{{ error }}</p>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
