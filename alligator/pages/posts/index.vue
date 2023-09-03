bd25325dd8825277588fa90d905b37fa8bdd85bc
takatoshi.tenko <chanten811@gmail.com>
2023-09-03 10:02:24 +0900

    fix: useLazyFetchの実装

--- alligator/pages/posts/index.vue
+++ alligator/pages/posts/index.vue
@@ -1,24 +1,19 @@
 <script setup>
-// const {
-//   data: posts,
-//   error,
-//   refresh,
-// } = await useFetch("https://jsonplaceholder.typicode.com/posts/");
-// console.log(error.value, "error");
-const { data: posts, error } = await useAsyncData("posts", () => {
-  console.log("fetch");
-  return $fetch("https://jsonplaceholder.typicode.com/posts/");
-});
-console.log(useNuxtApp().payload.data);
+  const {
+    data: posts,
+    error,
+    pending,
+  } = await useLazyFetch('https://jsonplaceholder.typicode.com/posts/');
 </script>
-
 <template>
   <div>
     <h1>Posts一覧</h1>
-    <button @click="refresh()">再取得</button>
     <p v-if="error">{{ error }}</p>
+    <p v-if="pending">Loading...</p>
     <ul>
-      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+      <li v-for="post in posts" :key="post.id">
+        <NuxtLink :to="`/posts/${post.id}`">{{ post.title }}</NuxtLink>
+      </li>
     </ul>
   </div>
 </template>
