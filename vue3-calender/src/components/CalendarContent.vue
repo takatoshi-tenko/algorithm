d89a9c5a20d8f87b4ac74a94ef3f588c6d9ba73a
takatoshi.tenko <chanten811@gmail.com>
2023-09-04 22:17:46 +0900

    add: CalendarContentの追加

---
+++ vue3-calender/src/components/CalendarContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Hello World</p>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  mounted() {
+    let data = moment().startOf("month");
+    const youbiNum = data.day();
+    console.log(youbiNum);
+  },
+};
+</script>
